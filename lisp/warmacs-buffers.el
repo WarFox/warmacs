e392e7c41620c78d38e5754e70629349437262dc
Deepu Mohan Puthrote <git@deepumohan.com>
2024-02-03 16:03:06 +0530

    fix: refactor buffer management options

--- lisp/warmacs-buffers.el
+++ lisp/warmacs-buffers.el
@@ -1,23 +1,30 @@
 ;; warmacs-buffers.el -*- lexical-binding: t; -*-
 
+;; Buffer management
+
 (elpaca nil ;; defer
+  (defun warmacs/switch-to-buffer (buffer)
+    "Switch to BUFFER."
+    (interactive
+     (consult-buffer))
+    (switch-to-buffer buffer))
+
+  (defun warmacs/scratch-buffer-other-window ()
+    "Open scratch buffer in a new window."
+    (interactive)
+    (switch-to-buffer-other-window (get-buffer-create "*scratch*")))
+
   (warmacs/leader-menu "Buffers" "b"
     "b" #'consult-buffer
     "d" #'kill-current-buffer
-    "p" #'previous-buffer
+    "e" #'erase-buffer
+    "m" #'((lambda () (interactive) (warmacs/switch-to-buffer "*Messages*")) :which-key "*Messages*")
     "n" #'next-buffer
-    "o" #'((lambda ()
-	     (interactive)
-	     (switch-to-buffer nil))
-	   :which-key "other-buffer")
-    "y" #'((lambda ()
-	     (interactive)
-	     (clipboard-kill-ring-save (point-min) (point-max)))
-	   :which-key "copy-buffer")
+    "o" #'((lambda () (interactive) (warmacs/switch-to-buffer nil)) :which-key "other-buffer")
+    "p" #'previous-buffer
+    "r" #'rename-buffer
     "s" #'scratch-buffer
-    "m" #'((lambda ()
-	     (interactive)
-	     (switch-to-buffer "*Messages*"))
-	   :which-key "*Messages*")))
+    "S" #'(warmacs/scratch-buffer-other-window :which-key "scratch-other-window")
+    "y" #'((lambda () (interactive) (clipboard-kill-ring-save (point-min) (point-max))) :which-key "copy-buffer")))
 
 (provide 'warmacs-buffers)
