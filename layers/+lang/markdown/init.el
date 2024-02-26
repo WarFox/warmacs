7dc3cb9c37a1c08b075f7677dccac7527d54bbfe
Deepu Mohan Puthrote <git@deepumohan.com>
2024-02-26 22:35:46 +0000

    fix: add markdown layer

---
+++ layers/+lang/markdown/init.el
@@ -0,0 +1,13 @@
+;; +lang/markdown/init.el -*- lexical-binding: t; -*-
+
+(use-package markdown-mode
+  :custom
+  (markdown-command "multimarkdown")
+  (markdown-enable-math t)
+  (markdown-enable-wiki-links t)
+  (markdown-asymmetric-header t)
+  (markdown-fontify-code-blocks-natively t)
+  (markdown-gfm-additional-languages '("sh"))
+  (markdown-gfm-uppercase-checkbox t))
+
+(provide '+lang/markdown/init)
