b43b88e20a21702ffd1a05c5847312b020082025
Deepu Mohan Puthrote <git@deepumohan.com>
2024-03-02 00:35:24 +0000

    fix: keybindings for scala layer

--- layers/+lang/scala/init.el
+++ layers/+lang/scala/init.el
@@ -3,10 +3,84 @@
 ;;
 ;; Scala Layer
 
-(use-package scala-mode)
+(use-package scala-mode
+  :custom
+  ;; Compatibility with `aggressive-indent'
+  (scala-indent:align-forms t)
+  (scala-indent:align-parameters t)
+  (scala-indent:default-run-on-strategy)
+  (scala-indent:operator-strategy)
+  :config
+  ;; Automatically insert asterisk in a comment when enabled
+  (defun scala/newline-and-indent-with-asterisk ()
+    (interactive)
+    (newline-and-indent)
+    (scala-indent:insert-asterisk-on-multiline-comment))
+
+  (defun scala/join-line ()
+    "Adapt `scala-indent:join-line' to behave more like evil's line join.
+
+`scala-indent:join-line' acts like the vanilla `join-line',
+joining the current line with the previous one. The vimmy way is
+to join the current line with the next.
+
+Try to move to the subsequent line and then join. Then manually move
+point to the position of the join."
+    (interactive)
+    (let (join-pos)
+      (save-excursion
+        (goto-char (line-end-position))
+        (unless (eobp)
+          (forward-line)
+          (call-interactively 'scala-indent:join-line)
+          (setq join-pos (point))))
+
+      (when join-pos
+        (goto-char join-pos))))
+
+  ;; display compilation buffer at the bottom
+  (defun scala/display-sbt-at-bottom (buffer args)
+    "Display a short buffer in a dedicated window at frame bottom.
+For use with `sbt:display-buffer-action'."
+    (set-window-dedicated-p
+     (display-buffer-at-bottom buffer (cons '(window-height . 12) args))
+     t))
+  (setq sbt:display-buffer-action
+        (list #'scala/display-sbt-at-bottom))
+
+  :general
+  (:states 'insert :keymaps 'scala-mode-map
+           "RET" #'scala/newline-and-indent-with-asterisk)
+  (:states 'normal :keymaps 'scala-mode-map
+           "J" #'scala/join-line)
+  
+  ;; (evil-define-key 'insert scala-mode-map
+  ;;   (kbd "RET") 'scala/newline-and-indent-with-asterisk)
+
+  ;; (evil-define-key 'normal scala-mode-map "J" 'scala/join-line)
+  )
 
 (use-package sbt-mode
   :after scala-mode
-  :commands (sbt-start sbt-command))
+  :commands (sbt-start sbt-command)
+  :config
+  ;; WORKAROUND: https://github.com/hvesalai/emacs-sbt-mode/issues/31
+  ;; allows for using SPACE in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+  ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+  (setq sbt:program-options '("-Dsbt.supershell=false"))
+  :init
+  (warmacs/local-leader-menu scala
+      "=" '(:ignore t :wk "format")
+      "==" '((lambda () (interactive) (sbt-command "scalafmtAll")) :wk "format-all")
+      "b" '(:ignore t :wk "build")
+      "b." #'sbt-hydra
+      "bb" #'sbt-command
+      "bc" '((lambda () (interactive) (sbt-command "compile")) :wk "compile")
+      "bt" '((lambda () (interactive) (sbt-command "test")) :wk "test")
+      "bT" '((lambda () (interactive) (sbt-command "Test / compile")) :wk "compile-test")))
 
 (provide '+lang/scala/init)
