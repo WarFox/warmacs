bacb9ad6a2a355e846e82f2f429ad470b947ca59
Deepu Mohan Puthrote <git@deepumohan.com>
2024-03-03 18:23:22 +0000

    fix: setup commands for deferred loading for projectile

--- lisp/warmacs-projects.el
+++ lisp/warmacs-projects.el
@@ -6,6 +6,37 @@
 (use-package projectile
   :custom
   (projectile-switch-project-action #'consult-projectile-find-file)
+  (projectile-sort-order #'recentf)
+  :init
+  (setq-default
+   projectile-cache-file (expand-file-name "projectile.cache"
+                                           warmacs-cache-dir)
+   projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld"
+                                                    warmacs-cache-dir))
+  :commands (projectile-ag
+             projectile-compile-project
+             projectile-dired
+             projectile-find-dir
+             projectile-find-file
+             projectile-find-tag
+             projectile-test-project
+             projectile-grep
+             projectile-invalidate-cache
+             projectile-kill-buffers
+             projectile-multi-occur
+             projectile-project-root
+             projectile-recentf
+             projectile-regenerate-tags
+             projectile-replace
+             projectile-replace-regexp
+             projectile-reset-known-projects
+             projectile-run-async-shell-command-in-root
+             projectile-run-shell-command-in-root
+             projectile-switch-project
+             projectile-switch-to-buffer
+             projectile-vc)
+  :config
+  (projectile-mode 1)
   :general
   (warmacs/leader-menu "Projects" "p"
     "!" #'projectile-run-shell-command-in-root
