978cb139ce43f67f7452c36f036cd89d985a64b0
Deepu Mohan Puthrote <git@deepumohan.com>
2024-03-16 10:32:33 +0000

    feat(lsp): add lsp support and setup lsp-completion-mode
    - lsp uses company by default, we use Corfu

--- lisp/warmacs-completions.el
+++ lisp/warmacs-completions.el
@@ -216,6 +216,8 @@
     "sb" #'(warmacs/consult-line-multi-project :wk "Search project buffers")
     "sB" #'(warmacs/consult-line-multi :wk "Search all buffers")
     "ss" #'(warmacs/consult-line :wk "Search in current buffer")
+    "sl" #'consult-line
+    "sL" #'consult-line-multi
     "sk" #'consult-keep-lines
     "rc" #'consult-complex-command
     "su" #'consult-focus-lines
@@ -273,20 +275,11 @@
   ;; (setq vertico-cycle t)
   )
 
+;; Show completions in a child frame overlay
 (use-package vertico-posframe
   :after vertico
   :config
   (vertico-posframe-mode 1))
-
-;; Optionally use the `orderless' completion style.
-(use-package orderless
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
 
 ;; Enable rich annotations using the Marginalia package
 (use-package marginalia
@@ -304,7 +297,7 @@
   ;; package.
   (marginalia-mode))
 
-;; Corfu
+;; Corfu - completion ui
 (use-package corfu
   :custom
   (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
@@ -334,7 +327,57 @@
   ;; `completion-at-point' is often bound to M-TAB.
   (setq tab-always-indent 'complete)
   :init
-  (global-corfu-mode))
+  (global-corfu-mode 1))
+
+;; Use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Add extensions
+(use-package cape
+  ;; Bind dedicated completion commands
+  ;; Alternative prefix keys: C-c p, M-p, M-+, ...
+  :bind (("C-c p p" . completion-at-point) ;; capf
+         ("C-c p t" . complete-tag)        ;; etags
+         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("C-c p h" . cape-history)
+         ("C-c p f" . cape-file)
+         ("C-c p k" . cape-keyword)
+         ("C-c p s" . cape-elisp-symbol)
+         ("C-c p e" . cape-elisp-block)
+         ("C-c p a" . cape-abbrev)
+         ("C-c p l" . cape-line)
+         ("C-c p w" . cape-dict)
+         ("C-c p :" . cape-emoji)
+         ("C-c p \\" . cape-tex)
+         ("C-c p _" . cape-tex)
+         ("C-c p ^" . cape-tex)
+         ("C-c p &" . cape-sgml)
+         ("C-c p r" . cape-rfc1345))
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  (add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
+  (add-to-list 'completion-at-point-functions #'cape-sgml)
+  (add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  (add-to-list 'completion-at-point-functions #'cape-dict)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  (add-to-list 'completion-at-point-functions #'cape-line))
+
 
 (use-package nerd-icons-corfu
   :after corfu
---
+++ lisp/warmacs-lsp.el
@@ -0,0 +1,110 @@
+;; warmacs-lsp -- LSP mode -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Configure LSP mode, dap mode, and other LSP related packages.
+
+;;; Code:
+
+;; LSP
+(use-package lsp-ui
+  :general
+  (:keymaps 'lsp-ui-mode-map
+            :states 'normal
+            "gd" #'lsp-ui-peek-find-definitions
+            "gr" #'lsp-ui-peek-find-references
+            "gD" #'lsp-ui-peek-jump-backward
+            "gR" #'lsp-ui-peek-jump-forward)
+  ([remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  ([remap xref-find-references] #'lsp-ui-peek-find-references))
+
+(use-package consult-lsp
+  :after (lsp-mode))
+
+(use-package lsp-treemacs
+  :after (lsp-mode treemacs))
+
+(use-package lsp-mode
+  :hook
+  (lsp-mode . lsp-enable-which-key-integration)
+  (lsp-completion-mode . warmacs/lsp-mode-setup-completion)
+  :custom
+  (lsp-completion-provider :none) ;; we use Corfu!
+  :init
+  (setq
+   lsp-server-install-dir (expand-file-name "lsp/" warmacs-cache-dir)
+   lsp-session-file (expand-file-name (file-name-nondirectory ".lsp-session-v1") lsp-server-install-dir)
+   lsp-eslint-library-choices-file (expand-file-name ".lsp-eslint-choices" lsp-server-install-dir)
+   lsp-yaml-schema-store-local-db (expand-file-name "lsp-yaml-schemas.json" lsp-server-install-dir))
+
+  ;; https://github.com/minad/corfu/wiki#configuring-corfu-for-lsp-mode
+  ;; Configure completions with orderless, cape and corfu.
+  (defun warmacs/orderless-dispatch-flex-first (_pattern index _total)
+    (and (eq index 0) 'orderless-flex))
+
+  (defun warmacs/lsp-mode-setup-completion ()
+    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+          '(orderless))
+    ;; Optionally configure the first word as flex filtered.
+    (add-hook 'orderless-style-dispatchers #'warmacs/orderless-dispatch-flex-first nil 'local)
+    ;; Optionally configure the cape-capf-buster.
+    (setq-local completion-at-point-functions (list (cape-capf-buster #'lsp-completion-at-point))))
+
+  :config
+  ;; This sets the lsp indentation for all modes derived from web-mode.
+  (add-to-list 'lsp--formatting-indent-alist '(web-mode . web-mode-markup-indent-offset))
+
+
+  (push '("*lsp-help*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
+        popwin:special-display-config)
+
+  :general
+  (warmacs/local-leader-menu lsp
+      ;; format
+      "=" '(:ignore t :wk "format")
+      "==" #'lsp-format-buffer
+      "=r" #'lsp-format-region
+      "=o" #'lsp-organize-imports
+      ;; code actions
+      "a" '(:ignore t :wk "code actions")
+      "aa" #'lsp-execute-code-action
+      ;; goto
+      ;; N.B. implementation and references covered by xref bindings / lsp provider...
+      "g" '(:ignore t :wk "goto")
+      "gt" #'lsp-find-type-definition
+      "gM" #'lsp-ui-imenu
+      ;; help
+      "h" '(:ignore t :wk "help")
+      "hh" #'lsp-describe-thing-at-point
+      ;; backend
+      "b" '(:ignore t :wk "backend")
+      "bd" #'lsp-describe-session
+      "br" #'lsp-workspace-restart
+      "bs" #'lsp-workspace-shutdown
+      "bv" #'lsp-version
+      ;; refactor
+      "r" '(:ignore t :wk "refactor")
+      "rr" #'lsp-rename
+      ;; toggles
+      "T" '(:ignore t :wk "toggle")
+      "Tl" '(:ignore t :wk "lsp")
+      "Tld" #'lsp-ui-doc-mode
+      "Tls" #'lsp-ui-sideline-mode
+      "Tll" #'lsp-lens-mode
+      ;; folders
+      "F" '(:ignore t :wk "folder")
+      "Fs" #'lsp-workspace-folders-switch
+      "Fr" #'lsp-workspace-folders-remove
+      "Fa" #'lsp-workspace-folders-add
+      ;; text/code
+      "x" '(:ignore t :wk "text/code")
+      "xh" #'lsp-document-highlight
+      "xl" #'lsp-lens-show
+      "xL" #'lsp-lens-hide))
+
+;; DAP
+
+(use-package dap-mode
+  :after lsp-mode
+  :config (dap-auto-configure-mode))
+
+(provide 'warmacs-lsp)
