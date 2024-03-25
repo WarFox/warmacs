38578374adb5fab793c7fadd84dc903c9e45496c
Deepu Mohan Puthrote <git@deepumohan.com>
2024-03-25 22:48:15 +0000

    fix: switch to general keybindings for warmacs-completions

--- lisp/warmacs-completions.el
+++ lisp/warmacs-completions.el
@@ -31,14 +31,14 @@
 
   ;; Configure other variables and modes in the :config section,
   ;; after lazily loading the package.
-  :config
+  :preface
 
   (defun warmacs/--active-region-or-thing-at-point ()
     "Get the active region or thing at point."
     (interactive)
     (if (region-active-p)
-	(buffer-substring-no-properties
-	 (region-beginning) (region-end))
+        (buffer-substring-no-properties
+         (region-beginning) (region-end))
       (or (thing-at-point 'symbol t) "")))
 
   (defun warmacs/consult-line ()
@@ -51,21 +51,21 @@
     "Search for matching line in project buffers with initial input based on selected region or symbol at point."
     (interactive)
     (consult-line-multi nil
-			(warmacs/--active-region-or-thing-at-point)))
+                        (warmacs/--active-region-or-thing-at-point)))
 
   (defun warmacs/consult-line-multi ()
     "Search for matching line in all buffers with initial input based on selected region or symbol at point."
     (interactive)
     (consult-line-multi t
-			(warmacs/--active-region-or-thing-at-point)))
+                        (warmacs/--active-region-or-thing-at-point)))
 
   (defun warmacs/search (use-initial-input initial-directory)
     "Search with initial input based on selected region or symbol at point."
     (let* ((initial-input (if use-initial-input
                               (warmacs/--active-region-or-thing-at-point)
                             ""))
-	   (default-directory (or initial-directory
-				  (read-directory-name "Start from directory: "))))
+           (default-directory (or initial-directory
+                                  (read-directory-name "Start from directory: "))))
       (consult-ripgrep default-directory initial-input)))
 
   (defun warmacs/search-auto ()
@@ -88,6 +88,7 @@
     (interactive)
     (warmacs/search-project-root))
 
+  :config
   ;; Optionally configure preview. The default value
   ;; is 'any, such that any key triggers the preview.
   ;; (setq consult-preview-key 'any)
@@ -127,58 +128,6 @@
   ;;;; 5. No project support
   ;; (setq consult-project-function nil)
 
-  :bind
-  (;; C-c bindings in `mode-specific-map'
-   ("C-c M-x" . consult-mode-command)
-   ("C-c h" . consult-history)
-   ("C-c k" . consult-kmacro)
-   ("C-c m" . consult-man)
-   ("C-c i" . consult-info)
-   ([remap Info-search] . consult-info)
-   ;; C-x bindings in `ctl-x-map'
-   ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-   ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-   ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-   ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-   ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
-   ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-   ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-   ;; Custom M-# bindings for fast register access
-   ("M-#" . consult-register-load)
-   ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-   ;; Other custom bindings
-   ;; M-g bindings in `goto-map'
-   ("M-g e" . consult-compile-error)
-   ("M-g f" . consult-flycheck)              ;; Alternative: consult-flymake
-   ("M-g g" . consult-goto-line)             ;; orig. goto-line
-   ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-   ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-   ("M-g m" . consult-mark)
-   ("M-g k" . consult-global-mark)
-   ("M-g i" . consult-imenu)
-   ("M-g I" . consult-imenu-multi)
-   ;; M-s bindings in `search-map'
-   ("M-s d" . consult-find)                  ;; Alternative: consult-fd
-   ("M-s c" . consult-locate)
-   ("M-s g" . consult-grep)
-   ("M-s G" . consult-git-grep)
-   ("M-s r" . consult-ripgrep)
-   ("M-s l" . consult-line)
-   ("M-s L" . consult-line-multi)
-   ("M-s k" . consult-keep-lines)
-   ("M-s u" . consult-focus-lines)
-   ;; Isearch integration
-   ("M-s e" . consult-isearch-history)
-   :map isearch-mode-map
-   ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-   ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-   ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-   ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-   ;; Minibuffer history
-   :map minibuffer-local-map
-   ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-   ("M-r" . consult-history))                ;; orig. previous-matching-history-element
-
   :general
   ([remap apropos]                       #'consult-apropos)
   ([remap bookmark-jump]                 #'consult-bookmark)
@@ -198,8 +147,61 @@
   ([remap projectile-find-file]          #'consult-projectile-find-file)
   ([remap yank-pop]                      #'consult-yank-pop)
   ([remap vc-git-grep]                   #'consult-git-grep)
+  ([remap Info-search]                   #'consult-info)
 
-  ("C-s" 'consult-line)
+  ("C-s" #'consult-line
+   ;; C-c bindings in `mode-specific-map'
+   "C-c M-x" #'consult-mode-command
+   "C-c h" #'consult-history
+   "C-c k" #'consult-kmacro
+   "C-c m" #'consult-man
+   "C-c i" #'consult-info
+   ;; C-x bindings in `ctl-x-map'
+   "C-x M-:" #'consult-complex-command     ;; orig. repeat-complex-command
+   "C-x b" #'consult-buffer                ;; orig. switch-to-buffer
+   "C-x 4b" #'consult-buffer-other-window ;; orig. switch-to-buffer-other-window
+   "C-x 5b" #'consult-buffer-other-frame  ;; orig. switch-to-buffer-other-frame
+   "C-x tb" #'consult-buffer-other-tab    ;; orig. switch-to-buffer-other-tab
+   "C-x rb" #'consult-bookmark            ;; orig. bookmark-jump
+   "C-x pb" #'consult-project-buffer      ;; orig. project-switch-to-buffer
+   ;; Custom M-# bindings for fast register access
+   "M-#" #'consult-register-load
+   "M-'" #'consult-register-store          ;; orig. abbrev-prefix-mark (unrelated)
+   ;; Other custom bindings
+   ;; M-g bindings in `goto-map'
+   "M-g e" #'consult-compile-error
+   "M-g f" #'consult-flycheck              ;; Alternative: consult-flymake
+   "M-g g" #'consult-goto-line             ;; orig. goto-line
+   "M-g M-g" #'consult-goto-line           ;; orig. goto-line
+   "M-g o" #'consult-outline               ;; Alternative: consult-org-heading
+   "M-g m" #'consult-mark
+   "M-g k" #'consult-global-mark
+   "M-g i" #'consult-imenu
+   "M-g I" #'consult-imenu-multi
+   ;; M-s bindings in `search-map'
+   "M-s d" #'consult-find                  ;; Alternative: consult-fd
+   "M-s c" #'consult-locate
+   "M-s g" #'consult-grep
+   "M-s G" #'consult-git-grep
+   "M-s r" #'consult-ripgrep
+   "M-s l" #'consult-line
+   "M-s L" #'consult-line-multi
+   "M-s k" #'consult-keep-lines
+   "M-s u" #'consult-focus-lines
+   ;; Isearch integration
+   "M-s e" #'consult-isearch-history)
+
+  (:keymaps
+   'isearch-mode-map
+   "M-e"   #'consult-isearch-history       ;; orig. isearch-edit-string
+   "M-s e" #'consult-isearch-history       ;; orig. isearch-edit-string
+   "M-s l" #'consult-line                  ;; needed by consult-line to detect isearch
+   "M-s L" #'consult-line-multi)            ;; needed by consult-line to detect isearch
+  ;; Minibuffer history
+  (:keymaps
+   'minibuffer-local-map
+   "M-s" #'consult-history                 ;; orig. next-matching-history-element
+   "M-r" #'consult-history)                ;; orig. previous-matching-history-element
 
   (warmacs/set-leader-keys
     "#" #'consult-register
@@ -234,10 +236,10 @@
   :after (consult flycheck))
 
 (use-package embark
-  :bind
-  (("C-." . embark-act)         ;; pick some comfortable binding
-   ("C-;" . embark-dwim)        ;; good alternative: M-.
-   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :general
+  ("C-." #'embark-act         ;; pick some comfortable binding
+   "C-;" #'embark-dwim        ;; good alternative: M-.
+   "C-h B" #'embark-bindings) ;; alternative for `describe-bindings'
 
   :init
 
@@ -286,8 +288,10 @@
   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
   ;; available in the *Completions* buffer, add it to the
   ;; `completion-list-mode-map'.
-  :bind (:map minibuffer-local-map
-              ("M-A" . marginalia-cycle))
+  :general-config
+  (:keymaps
+   'minibuffer-local-map
+   "M-A" #'marginalia-cycle)
 
   ;; The :init section is always executed.
   :init
@@ -343,23 +347,24 @@
 (use-package cape
   ;; Bind dedicated completion commands
   ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-  :bind (("C-c p p" . completion-at-point) ;; capf
-         ("C-c p t" . complete-tag)        ;; etags
-         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-         ("C-c p h" . cape-history)
-         ("C-c p f" . cape-file)
-         ("C-c p k" . cape-keyword)
-         ("C-c p s" . cape-elisp-symbol)
-         ("C-c p e" . cape-elisp-block)
-         ("C-c p a" . cape-abbrev)
-         ("C-c p l" . cape-line)
-         ("C-c p w" . cape-dict)
-         ("C-c p :" . cape-emoji)
-         ("C-c p \\" . cape-tex)
-         ("C-c p _" . cape-tex)
-         ("C-c p ^" . cape-tex)
-         ("C-c p &" . cape-sgml)
-         ("C-c p r" . cape-rfc1345))
+  :general
+  ("C-c dp" #'completion-at-point ;; capf
+   "C-c pt" #'complete-tag        ;; etags
+   "C-c pd" #'cape-dabbrev        ;; or dabbrev-completion
+   "C-c ph" #'cape-history
+   "C-c pf" #'cape-file
+   "C-c pk" #'cape-keyword
+   "C-c ps" #'cape-elisp-symbol
+   "C-c pe" #'cape-elisp-block
+   "C-c pa" #'cape-abbrev
+   "C-c pl" #'cape-line
+   "C-c pw" #'cape-dict
+   "C-c pr" #'cape-rfc1345
+   "C-c p :" #'cape-emoji
+   "C-c p \\" #'cape-tex
+   "C-c p _" #'cape-tex
+   "C-c p ^" #'cape-tex
+   "C-c p &" #'cape-sgml)
   :init
   ;; Add to the global default value of `completion-at-point-functions' which is
   ;; used by `completion-at-point'.  The order of the functions matters, the
@@ -399,7 +404,7 @@
 
   ;; Do not allow the cursor in the minibuffer prompt
   (setq minibuffer-prompt-properties
-	'(read-only t cursor-intangible t face minibuffer-prompt))
+        '(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
   ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
