a035a69ae5389893dccc7f6517987dc6f35fa7a3
Deepu Mohan Puthrote <git@deepumohan.com>
2024-04-01 10:56:49 +0100

    fix: use sharp quotes for clojure layer bindings; setup hooks

--- layers/+lang/clojure/init.el
+++ layers/+lang/clojure/init.el
@@ -7,7 +7,8 @@
 (use-package sesman)
 
 (use-package cider
-  :general
+  :hook ((clojure-mode clojure-ts-mode) . cider-mode)
+  :general-config
   (warmacs/set-local-leader-keys
     :keymaps '(clojure-mode-map clojure-ts-mode-map)
     "=" '(:ignore t :wk "format")
@@ -17,24 +18,24 @@
     "=r" #'cider-format-region
 
     "d" '(:ignore t :wk "debug")
-    "db" 'cider-debug-defun-at-point
+    "db" #'cider-debug-defun-at-point
     "de" '("cider-error-buffer" . (lambda () (interactive) (warmacs/switch-to-buffer cider-error-buffer)))
     "di" '(:ignore t :wk "inspect")
-    "die" 'cider-inspect-last-sexp
-    "dif" 'cider-inspect-defun-at-point
-    "dii" 'cider-inspect
-    "dil" 'cider-inspect-last-result
-    "div" 'cider-inspect-expr
+    "die" #'cider-inspect-last-sexp
+    "dif" #'cider-inspect-defun-at-point
+    "dii" #'cider-inspect
+    "dil" #'cider-inspect-last-result
+    "div" #'cider-inspect-expr
 
     "h" '(:ignore t :wk "help")
-    "ha" 'cider-apropos
-    "hc" 'cider-cheatsheet
-    "hd" 'cider-clojuredocs
-    "hj" 'cider-javadoc
-    "hn" 'cider-browse-ns
-    "hN" 'cider-browse-ns-all
-    "hs" 'cider-browse-spec
-    "hS" 'cider-browse-spec-all
+    "ha" #'cider-apropos
+    "hc" #'cider-cheatsheet
+    "hd" #'cider-clojuredocs
+    "hj" #'cider-javadoc
+    "hn" #'cider-browse-ns
+    "hN" #'cider-browse-ns-all
+    "hs" #'cider-browse-spec
+    "hS" #'cider-browse-spec-all
 
     "e" '(:ignore t :wk "eval")
     "e;" #'cider-eval-defun-to-comment
@@ -54,20 +55,20 @@
     "er" #'cider-eval-region
 
     "n" '(:ignore t :wk "namespace")
-    "nn" 'cider-ns-reload
-    "nN" 'cider-ns-reload-all
+    "nn" #'cider-ns-reload
+    "nN" #'cider-ns-reload-all
 
     "'"  '("sesman-start" . sesman-start)
     "s"  '(:ignore t :wk "session")
-    "sq" 'cider-quit
-    "sr" 'cider-restart
+    "sq" #'cider-quit
+    "sr" #'cider-restart
     "sn" #'cider-repl-set-ns
     "sb" #'cider-switch-to-repl-buffer
     "sc" #'cider-connect
     "ss" #'cider-switch-to-last-clojure-buffer))
 
 (use-package clj-refactor
-  ;; :hook (clojure-mode . clj-refactor-mode)
+  :hook ((clojure-mode clojure-ts-mode) . clj-refactor-mode)
   :preface
   (add-to-list 'elpaca-ignored-dependencies 'inflections)
   :init
@@ -78,7 +79,7 @@
   ;; here because it reads the variable `cljr--all-helpers'. Since
   ;; `clj-refactor-mode' is added to the hook, this should trigger when a
   ;; clojure buffer is opened anyway, so there's no "keybinding delay".
-  :general
+  :general-config
   ;; keybindings for refactoring
   (warmacs/set-local-leader-keys
     :keymaps '(clojure-mode-map clojure-ts-mode-map)
