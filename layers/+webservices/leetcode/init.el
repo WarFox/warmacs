ea15cf31c95c15edf64ca89777a5066f371b3f1a
Deepu Mohan Puthrote <git@deepumohan.com>
2024-04-04 22:55:24 +0100

    feat: add new layer +webservices/leetcode

---
+++ layers/+webservices/leetcode/init.el
@@ -0,0 +1,21 @@
+;; +webservices/leetcode/init.el -*- lexical-binding: t; -*-
+
+(use-package leetcode
+  :commands (leetcode)
+  :general
+  (warmacs/leader-menu "Webservices" "W"
+    "L" '(:ignore t :wk "LeetCode")
+    "Ll" #'leetcode)
+  :general-config
+  (general-def
+    :keymaps 'leetcode--problems-mode-map
+    "TAB" #'leetcode-show-current-problem
+    "<return>" #'leetcode-show-current-problem)
+  (warmacs/leader-menu "Webservices" "W"
+    "Ld" #'leetcode-show-current-problem
+    "Lr" #'leetcode-refresh
+    "Lt" #'leetcode-try
+    "Lu" #'leetcode-submit
+    "Lq" #'leetcode-quit))
+
+(provide '+webservices/leetcode/init)
