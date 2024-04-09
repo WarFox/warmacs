d867958bb13c122bef40a30e437226477b46afe6
Deepu Mohan Puthrote <git@deepumohan.com>
2024-04-09 21:50:14 +0100

    fix: resolve conflict with built-in 'restart-emacs function in files
    - also removs unimplemented options --timed-requires and --adv-timers

--- lisp/warmacs-restart.el
+++ lisp/warmacs-restart.el
@@ -3,29 +3,30 @@
 ;; Quit/Restart menu
 
 (use-package restart-emacs
-  :commands (restart-emacs kill-emacs)
+  :commands (restart-emacs restart-emacs-start-new-emacs)
   :preface
   (defun warmacs/restart-emacs (&optional args)
     (interactive)
-    (restart-emacs))
+    (restart-emacs args))
 
   (defun warmacs/kill-emacs (prompt &optional args)
     (interactive)
     (if (not prompt)
         (save-some-buffers nil t))
     (kill-emacs args))
+  :after
+  (files)
+  :init
+  ;; unbind 'restart-emacs to avoid conflict with built-in function in files.el
+  ;; https://github.com/syl20bnr/spacemacs/pull/16186/files
+  (fmakunbound 'restart-emacs)
+  (autoload 'restart-emacs "restart-emacs" nil t)
   :general
   (warmacs/leader-menu "quit" "q"
     "d" '("restart-emacs-debug-init" . (lambda (&optional args)
                                          (interactive)
                                          (warmacs/restart-emacs (cons "--debug-init" args))))
     "R" #'warmacs/restart-emacs
-    "t" '("restart-emacs-timed-requires" . (lambda (&optional args)
-                                             (interactive)
-                                             (warmacs/restart-emacs (cons "--timed-requires" args))))
-    "T" '("restart-emacs-adv-timers" . (lambda (&optional args)
-	                                 (interactive)
-                                         (warmacs/restart-emacs (cons "--adv-timers" args))))
     "q" '("prompt-kill-emacs" . (lambda (&optional args)
 	                          (interactive)
                                   (warmacs/kill-emacs t args)))))
