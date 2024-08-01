9fac5dafa912125bfc14c38ff71cfe4efe203981
Deepu Mohan Puthrote <git@deepumohan.com>
2024-08-01 23:42:57 +0100

    fix(typescript): add npm related keybindings for typescript layer

--- layers/+lang/typescript/init.el
+++ layers/+lang/typescript/init.el
@@ -1,8 +1,21 @@
 ;; +lang/typescript/init.el -*- lexical-binding: t; -*-
 
 (use-package typescript-mode
-  :mode "\\.ts\\'"
+  :mode ("\\.ts\\'" . typescript-ts-mode)
   :hook
-  ((typescript-mode typescript-ts-mode) . lsp-deferred))
+  ((typescript-ts-mode typescript-tsx-mode) . lsp-deferred)
+  ((typescript-ts-mode typescript-tsx-mode) . npm-mode)
+  :general-config
+  (warmacs/set-local-leader-keys
+    :keymaps '(typescript-ts-mode-map typescript-tsx-mode-map)
+    "n" '(:ignore t :wk "npm")
+    "ni" 'npm-mode-npm-install
+    "nr" 'npm-mode-npm-run
+    "ns" 'npm-mode-npm-install-save
+    "nd" 'npm-mode-npm-install-save-dev
+    "nn" 'npm-mode-npm-init
+    "nu" 'npm-mode-npm-uninstall
+    "nl" 'npm-mode-npm-list
+    "np" 'npm-mode-visit-project-file))
 
 (provide '+lang/typescript/init)
